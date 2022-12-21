33a2507b6440bdf815eb4e7eccf9e6ed4a75cd4a
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-12-21 14:24:48 +0700

    Add files via upload

---
+++ kode.58.gaya.py
@@ -0,0 +1,15 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+m = float(input("Masukan Nilai Massa Benda (m) : "))
+a = float(input("Masukan Nilai Percepatan (a): "))
+
+# Mengkonversi
+f = m * a
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Gaya yang dihasilkan = ',f)
+print('==========================================================')
